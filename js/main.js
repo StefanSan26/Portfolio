23d42501b7ac04aa68002321feb5cfa9e59e3c31
stefan <stefanace26@gmail.com>
2021-08-22 14:07:42 -0400

    little adjust

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const navOpen = document.getElementById("nav-open")
 const navMenu = document.getElementById("nav-menu")
+const navLink = document.querySelectorAll(".nav__link")
+console.log(navLink)
 
-function openMenu(){
+function switchMenu(){
 	navMenu.classList.toggle('show')
 }
 
-navOpen.addEventListener("click",openMenu)
+navOpen.addEventListener("click",switchMenu);
+navLink.forEach(link => link.addEventListener("click",switchMenu))
