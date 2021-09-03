1721b7529062c40ae95f8bb12c100d6d798d5da6
stefan <stefanace26@gmail.com>
2021-09-03 16:18:49 -0400

    progressing

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const navOpen = document.getElementById("nav-open")
 const navMenu = document.getElementById("nav-menu")
 const navLink = document.querySelectorAll(".nav__link")
-console.log(navLink)
 
 function switchMenu(){
 	navMenu.classList.toggle('show')
+	navMenu.style.transition = '0.5s'
 }
 
-navOpen.addEventListener("click",switchMenu);
+navOpen.addEventListener("click",switchMenu)
+
+
 navLink.forEach(link => link.addEventListener("click",switchMenu))
+
